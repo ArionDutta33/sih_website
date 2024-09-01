fa7c54cbed9afcd673e1db8233674a8f9ebb94f8
ArionDutta33 <ariondutta333@outlook.com>
2024-09-02 00:41:51 +0530

    done

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,7 @@
-const arrowDown = document.querySelector(".arrow-down")
-console.log(arrowDown)
+const container = document.querySelector(".container")
+window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 200) {
+        const arrowDown = document.querySelector(".arrow-down")
+        arrowDown.classList.remove("hidden")
+    }
+})
