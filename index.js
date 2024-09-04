e387d2f4b68820c03b4a0e943f7cc4cf5b04cc84
ArionDutta33 <ariondutta333@outlook.com>
2024-09-04 11:53:13 +0530

    Multer and cloudinary

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const ejsMate = require("ejs-mate")
 const path = require("path")
+const { cloudinary } = require("./utils/cloudinary");
+const multer = require('multer');
+const { storage } = require('./utils/cloudinary');
+const upload = multer({ storage });
+
+//
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -15,6 +25,13 @@ app.get("/", (req, res) => {
 app.get("/upload", (req, res) => {
     res.render("upload")
 })
+
+app.post("/upload", upload.single("deepfake"), (req, res) => {
+    console.log(req.body)
+})
+
+
+
 app.listen(3000, () => {
     console.log("server up on 3000")
 })
